93344ba36a6d037c085e9f74bba7e6b651401467
svetultus <svetultus@gmail.com>
2019-07-28 13:18:07 +0300

    drag and drop and click joined

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 import Controller from './mvc/controller';
 let init = require ('../init.json');
 
-let container = document.querySelector("#friendsFilter"),
-    zoneLeft = document.querySelector('#friendsList'),
-    zoneRight = document.querySelector('#friendsListFiltered'),
-    friendFilterListClass='friends-filter__list',
-    headerInfo = document.querySelector("#headerInfo"),
-    apiId = init.VK.apiId,
-    templateFriends = document.querySelector('#templateFriends').textContent,
-    resultFriends = document.querySelector('#friendsList');
+const options = {
+    container: document.querySelector("#friendsFilter"),
+    zoneLeft: document.querySelector('#friendsList'),
+    zoneRight: document.querySelector('#friendsListFiltered'),
+    friendFilterListClass: 'friends-filter__list',
+    headerInfo: document.querySelector("#headerInfo"),
+    apiId: init.VK.apiId,
+    templateFriends: document.querySelector('#templateFriends').textContent,
+    list1: document.querySelector('#friendsList1 .friends-filter__list'),
+    list2: document.querySelector('#friendsList2 .friends-filter__list'),
+    form1: document.querySelector('#friends-filter__form_1'),
+    form2: document.querySelector('#friends-filter__form_2')
+}
 
-const controller = new Controller(apiId, container, headerInfo, templateFriends, resultFriends, friendFilterListClass, zoneLeft, zoneRight);
+const controller = new Controller(options);
--- js/mvc/controller.js
+++ js/mvc/controller.js
@@ -1,15 +1,20 @@
 const VKAPI = require('../modules/api.vk');
 
 export default class {
-    constructor(apiId, container, headerInfo, templateFriends, resultFriends, friendFilterListClass, zoneLeft, zoneRight) {
-        this.container = container;
-        this.zoneLeft = zoneLeft;
-        this.zoneRight = zoneRight;
-        this.apiId = apiId;
-        this.headerInfo = headerInfo;
-        this.templateFriends = templateFriends;
-        this.resultFriends = resultFriends;
-        this.friendFilterListClass = friendFilterListClass;
+    constructor(options) {
+        this.container = options.container;
+        this.zoneLeft = options.zoneLeft;
+        this.zoneRight = options.zoneRight;
+        this.apiId = options.apiId;
+        this.headerInfo = options.headerInfo;
+        this.templateFriends = options.templateFriends;
+        this.list1 = options.list1;
+        this.list2 = options.list2;
+        this.form1 = options.form1;
+        this.form2 = options.form2;
+        this.friendFilterListClass = options.friendFilterListClass;
+        this.friendsList1 = [];
+        this.friendsList2 = [];
 
         this.dragStartHandler = this.dragStartHandler.bind(this);
         this.dragEndHandler = this.dragEndHandler.bind(this);
@@ -19,6 +24,7 @@ export default class {
         this.getItem = this.getItem.bind(this);
         this.clickHandler = this.clickHandler.bind(this);
         this.moveItem = this.moveItem.bind(this);
+        this.moveData = this.moveData.bind(this);
 
         this.VKobj = new VKAPI;
 
@@ -26,7 +32,8 @@ export default class {
         .then (()=>{
             this.VKobj.getUserData(this.headerInfo);
             this.VKobj.getUserFriends().then((friends)=>{
-                this.friends = friends.response.items;
+                this.friendsList1 = friends.response.items;
+                this.friendsList1 = this.friendsList1.slice(0,5);
                 this.renderFriends();
             });
             
@@ -39,20 +46,63 @@ export default class {
         this.container.addEventListener('dragend', this.dragEndHandler, true);
         this.container.addEventListener('drop', this.dropHandler, true);
         this.container.addEventListener('dragover', this.dragOverHandler, true);
-        this.container.addEventListener('click', this.clickHandler, true)
+        this.container.addEventListener('click', this.clickHandler, true);
+        this.container.addEventListener('submit', this.submitHandler, true);
     }
 
     renderFriends() {
-        let friends = this.friends.slice(0, 5);
-        let render = Handlebars.compile(this.templateFriends);
-        let html = render(friends);
-        this.resultFriends.innerHTML = html;
+        let html,
+            render = Handlebars.compile(this.templateFriends);
+        
+        html = render({friends: this.friendsList1, btnClass: 'add'});
+        this.list1.innerHTML = html;
+        html = render({friends: this.friendsList2, btnClass: 'remove'});
+        this.list2.innerHTML = html;
+
     }
     getZone (elem) {
         return elem.closest('.friends-filter__inner');
     }
     getItem (elem) {
         return elem.closest('.friends-filter__list-elem');
+    }
+    moveData (id, e) {
+        let itemId = Number(id),
+            itemMoved,
+            index,
+            listFrom,
+            listTo;
+
+        if (e.type === 'click' && e.target.classList.contains('friends-filter__list-btn_add')) {
+            console.log('добавить');
+            //zone = this.zoneRight;
+            listFrom = this.friendsList1;
+            listTo = this.friendsList2;
+            
+        } else if (e.type === 'click' && e.target.classList.contains('friends-filter__list-btn_remove')) {
+            console.log('удалить');
+            listFrom = this.friendsList2;
+            listTo = this.friendsList1;
+            // zone = this.zoneLeft;
+        }
+        if (e.type === 'drop' && (this.startDragArea !== this.getZone(e.target))) {
+            if (this.startDragArea.getAttribute('id') === 'friendsList1') {
+                listFrom = this.friendsList1;
+                listTo = this.friendsList2;
+            }
+            else if (this.startDragArea.getAttribute('id') === 'friendsList2') {
+                listFrom = this.friendsList2;
+                listTo = this.friendsList1;
+            }
+        }
+        
+        listFrom.forEach((elem, i) => {
+            if (elem.id === itemId) index = i;
+        })
+        
+        itemMoved = listFrom.splice(index,1);
+        listTo.push(itemMoved[0]); 
+        //console.log(listSrc, listTarget, itemId);
     }
 
     dragStartHandler(e) {
@@ -72,9 +122,12 @@ export default class {
         //console.log(e.target);
         let zone = this.getZone (e.target);
         if (zone && this.startDragArea !== zone) {
-            this.moveItem (this.draggingItem, zone);
+            //this.moveItem (this.draggingItem, zone);
+            this.moveData(this.draggingItem.getAttribute('data-id'), e);
+            this.renderFriends();
         }
     }
+
     moveItem (item, zone) {
         let btn = item.querySelector('.' + this.friendFilterListClass + '-btn');
         let ul = zone.querySelector('.' + this.friendFilterListClass);
@@ -88,20 +141,19 @@ export default class {
         e.preventDefault();
         //console.log(e.target);
     }
+
     clickHandler (e) {
-        let zone,
-            item = this.getItem(e.target);
+        let item = this.getItem(e.target);
 
         if (!item || !e.target.classList.contains('friends-filter__list-btn')) return; 
+        //this.moveItem (item, zone);
+        this.moveData(item.getAttribute('data-id'), e);
+        this.renderFriends();
+    }
 
-        if (e.target.classList.contains('friends-filter__list-btn_add')) {
-            console.log('добавить');
-            zone = this.zoneRight;
-            
-        } else if (e.target.classList.contains('friends-filter__list-btn_remove')) {
-            console.log('удалить');
-            zone = this.zoneLeft;
-        }
-        this.moveItem (item, zone);
+    submitHandler(e) {
+        e.preventDefault();
+
+        console.log(e.target);
     }
 }
